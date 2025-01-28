41b074564f66701a7a2aaec4436ecf2a6b69db2b
Gleb Zimin <gzimin@mirantis.com>
2025-01-28 12:51:15 +0100

    test

--- controllers/cassandracluster/generator.go
+++ controllers/cassandracluster/generator.go
@@ -307,8 +307,8 @@ func generateVolumeClaimTemplate(cc *api.CassandraCluster, labels map[string]str
 func generateJMXConfiguration(jmxConf api.JMXConfiguration) v1.EnvVar {
 	var jmxEnvVar v1.EnvVar
 	var jmxParam string
-	values := reflect.ValueOf(jmxConf).Elem()
-	types := reflect.TypeOf(jmxConf).Elem()
+	values := reflect.ValueOf(&jmxConf).Elem()
+	types := reflect.TypeOf(&jmxConf).Elem()
 	logrus.Errorf("INITIAL JMX CONFIGURATION %v\n", jmxConf)
 	logrus.Errorf("INITIAL VALUE: ", values)
 	for i := 0; i < values.NumField(); i++ {
