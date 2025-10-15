b2c1a4bdf9995ea6c617d632cf53d2e3bd9f19a8
Gleb Zimin <gzimin@mirantis.com>
2025-10-15 14:09:26 +0200

    [TEST]Use JMX Exporter
    Cassandra 4.x compatibility
    
    Change-Id: I5c0775c2a4b893ad62b1ee97b869a4110e8fadba

---
+++ test.sh
@@ -0,0 +1,3 @@
+EXPORTER_VERSION=0.9.16
+
+wget -O /tmp/cassandra-exporter-agent-${EXPORTER_VERSION}.jar https://github.com/gzimin/cassandra-exporter/releases/download/v${EXPORTER_VERSION}/cassandra-exporter-agent-${EXPORTER_VERSION}-SNAPSHOT.jar
